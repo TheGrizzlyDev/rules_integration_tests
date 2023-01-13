5806981867dd2b1d9a98ebe9a01fa34aff5dbe46
Antonio <tonio9681@gmail.com>
2023-01-13 14:55:43 +0000

    Testing out editor integration

--- core/go/main.go
+++ core/go/main.go
@@ -13,12 +13,18 @@ var port = flag.Int("port", 12345, "")
 
 func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	_, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	var opts []grpc.ServerOption
-	grpcServer := grpc.NewServer(opts...)
-	pb.RegisterRouteGuideServer(grpcServer, newServer())
-	grpcServer.Serve(lis)
+
+	_ = pb.InspectRequest{
+		Id: &pb.ContainerId{
+			Id: "",
+		},
+	}
+	// var opts []grpc.ServerOption
+	// grpcServer := grpc.NewServer(opts...)
+	// pb.RegisterRouteGuideServer(grpcServer, newServer())
+	// grpcServer.Serve(lis)
 }
