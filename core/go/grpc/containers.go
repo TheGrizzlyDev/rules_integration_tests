fbac00cc8f52e3add367383e640fa8bf507c0d6c
TheGrizzlyDev <tonio9681@gmail.com>
2023-01-28 19:51:39 +0000

    Start implementing ContainerManagerServer

--- core/go/grpc/containers.go
+++ core/go/grpc/containers.go
@@ -1,6 +1,11 @@
 package grpc
 
 import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	pb "github.com/TheGrizzlyDev/rules_integration_tests/proto/containers"
 )
 
@@ -10,4 +15,16 @@ type containerManagerServer struct {
 
 func newContainerManagerServer() (*containerManagerServer, error) {
 	return &containerManagerServer{}, nil
+}
+
+func (c *containerManagerServer) Start(context.Context, *pb.StartRequest) (*pb.StartResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Start not implemented")
+}
+
+func (c *containerManagerServer) Kill(context.Context, *pb.KillRequest) (*pb.KillResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Kill not implemented")
+}
+
+func (c *containerManagerServer) Inspect(context.Context, *pb.InspectRequest) (*pb.InspectResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Inspect not implemented")
 }
